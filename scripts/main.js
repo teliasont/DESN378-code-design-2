6e7d8821a75c1cd57663a54002030792e0c8f69a
teliasont <165819949+teliasont@users.noreply.github.com>
2026-01-08 00:42:09 +0000

    Add JavaScript alert to main.js and update index.html comments

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,8 @@
 
 console.log('Portfolio loaded');
 
+alert("Hello World!\n\nI'm JavaScript :)");
+
 // Week 1: You'll add theme toggle code here
 // Week 2: You'll add localStorage persistence here
 // Week 3+: More to come...
