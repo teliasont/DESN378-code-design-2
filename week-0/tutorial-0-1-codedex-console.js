f8982008fec8c3c05a00764f5005842f9f2382ce
teliasont <165819949+teliasont@users.noreply.github.com>
2026-01-08 02:03:03 +0000

    Update Learning Log link in README.md and add tutorial 0-1 codedex console exercises

---
+++ week-0/tutorial-0-1-codedex-console.js
@@ -0,0 +1,57 @@
+// ============================================
+// TUTORIAL 0-1: CODEDEX CONSOLE FUNDAMENTALS
+// Student: Tayla Eliason
+// Date: 01-07-2026
+// =======================================
+​
+// --------------------------------------------
+// EXERCISE 1: SETTING UP
+// --------------------------------------------
+console.log("Hello internet!");
+// Add your custom greeting below:
+​console.log("Obi-Wan: Hello there.")
+console.log("Obi-Wan: Hello there.")
+console.log("Obi-Wan: Hello there.\n")
+console.log("General Grevous: Several Kenobi.")
+
+​
+// --------------------------------------------
+// EXERCISE 2: THE CONSOLE
+// Your favorite food
+// --------------------------------------------
+​console.log("Pad Thai :P");
+​
+// --------------------------------------------
+// EXERCISE 3: LETTER TREE
+// The pattern: a, b c, d e f, g h i j, k
+// --------------------------------------------
+​console.log("   a");
+console.log("  b c");
+console.log(" d e f");
+console.log("g h i j");
+console.log("   k")
+​
+// --------------------------------------------
+// EXERCISE 4: SECRET RECIPE
+// Use comments to document your recipe
+// Include a "secret ingredient" comment
+// --------------------------------------------
+​
+​
+// --------------------------------------------
+// EXERCISE 5: RECEIPT
+// Format a receipt from a recent purchase
+// --------------------------------------------
+​console.log("==================================");
+console.log("         McDONALD'S RECEIPT");
+console.log("----------------------------------")
+console.log("           Order Details");
+console.log("----------------------------------");
+console.log("Item           Quantity   Price");
+console.log("----------------------------------");
+console.log("🍔 Burger       1          2.99");
+console.log("🍟 Fries        1          2.49");
+console.log("🥤 Soda         1          1.99");
+console.log("----------------------------------");
+console.log("Total                      7.47");
+console.log("==================================");
