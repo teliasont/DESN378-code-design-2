899ee2d6164233ad8ef5e3384c079d3801cd3406
teliasont <165819949+teliasont@users.noreply.github.com>
2026-01-09 03:32:14 +0000

    Fix typos and formatting in tutorial 0-1 codedex console exercises

--- week-0/tutorial-0-1-codedex-console.js
+++ week-0/tutorial-0-1-codedex-console.js
@@ -3,46 +3,58 @@
 // Student: Tayla Eliason
 // Date: 01-07-2026
 // =======================================
-​
+
 // --------------------------------------------
 // EXERCISE 1: SETTING UP
 // --------------------------------------------
 console.log("Hello internet!");
 // Add your custom greeting below:
-​console.log("Obi-Wan: Hello there.")
+console.log("Obi-Wan: Hello there.")
 console.log("Obi-Wan: Hello there.")
 console.log("Obi-Wan: Hello there.\n")
-console.log("General Grevous: Several Kenobi.")
+console.log("General Grievous: Several Kenobi.")
 
-​
 // --------------------------------------------
 // EXERCISE 2: THE CONSOLE
 // Your favorite food
 // --------------------------------------------
-​console.log("Pad Thai :P");
-​
+console.log("Pad Thai :P");
+
 // --------------------------------------------
 // EXERCISE 3: LETTER TREE
 // The pattern: a, b c, d e f, g h i j, k
 // --------------------------------------------
-​console.log("   a");
+console.log("   a");
 console.log("  b c");
 console.log(" d e f");
 console.log("g h i j");
 console.log("   k")
-​
+
 // --------------------------------------------
 // EXERCISE 4: SECRET RECIPE
 // Use comments to document your recipe
 // Include a "secret ingredient" comment
 // --------------------------------------------
-​
-​
+console.log("Soylent Green Mac and Cheese");
+console.log("Buy 1 microwavable cup of mac and cheese from your local seller.");
+console.log("Open the cup and remove the cheesy powder.");
+console.log("add water to the fill line.");
+console.log("Microwave for 3 minutes and 30 seconds.");
+console.log("Remove from microwave and mix in cheesy powder. Stir well.");
+//For extra flavor, add a dash of your favorite green plankton!
+/*DISCLAIMER:
+  The plankton is 100% safe to eat. 
+  It is not made of people.
+  Stop questioning it.
+*/
+console.log("Serve with luxuries like a teaspoon of strawberry jam or nutritious protein rich hummus alongside the Soylent Corporation’s new line of long pork rinds.");
+console.log("\n* Ingredients subject to change without notice depending on the quality of the annual plankton harvest.");
+
 // --------------------------------------------
 // EXERCISE 5: RECEIPT
 // Format a receipt from a recent purchase
 // --------------------------------------------
-​console.log("==================================");
+console.log("==================================");
 console.log("         McDONALD'S RECEIPT");
 console.log("----------------------------------")
 console.log("           Order Details");
