9cd1573e145cd38a744711f9edf14fa85c794012
teliasont <165819949+teliasont@users.noreply.github.com>
2026-02-04 03:57:20 +0000

    Updates to localStorage collection.

--- week-1/portfolio/scripts/scripts.js
+++ week-1/portfolio/scripts/scripts.js
@@ -1,6 +1,12 @@
-const toggle = document.querySelector(".theme-toggle");
+const themeToggle = document.querySelector(".theme-toggle");
 
-toggle.addEventListener("click", function () {
+const savedTheme = localStorage.getItem('theme');
+
+if (savedTheme) {
+  document.documentElement.dataset.theme = savedTheme;
+}
+
+themeToggle.addEventListener("click", function () {
 	const currentTheme = document.documentElement.dataset.theme;
 	let newTheme; 
 	if (currentTheme === "dark") {
@@ -9,4 +15,25 @@ toggle.addEventListener("click", function () {
 		newTheme = "dark";
 	}
 	document.documentElement.dataset.theme = newTheme;
+	localStorage.setItem('theme', newTheme);
 });
+/*
+const motionToggle = document.querySelector(".motion-toggle");
+const savedMotionPreference = localStorage.getItem('motionPreference');
+
+if (savedMotionPreference) {
+  document.documentElement.dataset.theme = savedMotionPreference;
+}
+
+motionToggle.addEventListener("click", function () {
+	const currentMotionPreference = document.documentElement.dataset.motionPreference;
+	let newMotionPreference; 
+	if (currentMotionPreference === "reduced") {
+		newMotionPreference = "no-preference";
+	} else {
+		newMotionPreference = "no-preference";
+	}
+	document.documentElement.dataset.theme = newMotionPreference;
+	localStorage.setItem('motionPreference', newMotionPreference);
+});
+*/
